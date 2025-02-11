0cdecde13cf0dcfb108fc4d717c1cf9e56229436
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-11 17:43:28 +0100

    aggiunto create e put

--- routers.js
+++ routers.js
@@ -10,3 +10,13 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   res.send(`Dettaglio del post ${req.params.id}`);
 });
+
+// Rotta per creare un nuovo post
+router.post("/", (req, res) => {
+  res.send("Creazione di un nuovo post");
+});
+
+// Rotta per aggiornare un post esistente
+router.put("/:id", (req, res) => {
+  res.send(`Aggiornamento del post ${req.params.id}`);
+});
